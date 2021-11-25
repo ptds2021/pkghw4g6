eec1b670a754ae5682bedcf64eabbd3905755603
csteffe <colin.steffe@unil.ch>
2021-11-25 10:28:17 +0100

    modified package pkghw4gN to pkghw4g6

--- inst/shiny-examples/area/runDemo.R
+++ inst/shiny-examples/area/runDemo.R
@@ -1,11 +1,11 @@
 #' @export
 runDemo <- function() {
   # REPLACE N BY YOUR GROUP NUMBER AND DELETE THIS COMMENT
-  appDir <- system.file("shiny-examples", "area", package = "pkghw4gN")
+  appDir <- system.file("shiny-examples", "area", package = "pkghw4g6")
   if (appDir == "") {
     stop(
       # REPLACE N BY YOUR GROUP NUMBER AND DELETE THIS COMMENT
-      "Could not find example directory. Try re-installing pkghw4gN.",
+      "Could not find example directory. Try re-installing pkghw4g6.",
       call. = FALSE
     )
   }
