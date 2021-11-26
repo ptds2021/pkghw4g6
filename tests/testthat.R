eb2543adc81ae13b369c56e82a58223c041cbb08
csteffe <colin.steffe@unil.ch>
2021-11-26 15:11:05 +0100

    automatic test with testthat for estimate_area
    - one test with right input
    - on test with wrong input

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,11 @@ library(testthat)
 library(pkghw4g6)
 
 test_check("pkghw4g6")
+
+# Examples
+
+library(testthat)
+test_that("check function with right input and wrong input"{
+  estimate_area(B = 4367, seed = 10)
+  estimate_area(B=bca,seed=csv)
+})
