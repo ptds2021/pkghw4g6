4dc76c68725d328bd5b5e9ff1a7281d1ade88378
JasmineBM <jasmine.mawjee@unil.ch>
2021-11-26 16:19:26 +0100

    runDemo.R
    Replace N by group number

--- inst/shiny-examples/area/runDemo.R
+++ inst/shiny-examples/area/runDemo.R
@@ -1,10 +1,10 @@
 #' @export
 runDemo <- function() {
-  # REPLACE N BY YOUR GROUP NUMBER AND DELETE THIS COMMENT
+
   appDir <- system.file("shiny-examples", "area", package = "pkghw4g6")
   if (appDir == "") {
     stop(
-      # REPLACE N BY YOUR GROUP NUMBER AND DELETE THIS COMMENT
+
       "Could not find example directory. Try re-installing pkghw4g6.",
       call. = FALSE
     )
