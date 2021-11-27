6c143c82ba93710a8df9ae1266d823eb66569ab8
JasmineBM <jasmine.mawjee@unil.ch>
2021-11-27 13:02:48 +0100

    correction

--- tests/testthat/test-area.R
+++ tests/testthat/test-area.R
@@ -4,3 +4,9 @@ test_that("pkghw4g6 works", {
   plot_shape <- pkghw4g6::plot(area)
   expect_visible(plot_shape)
 })
+
+# Test whether the estimate_area function returns a value and the data frame
+test_that("an error message is provided if wrong inputs", {
+  area <- pkghw4v2::estimate_area()
+  expect_condition_matching(area, "Error: wrong input provided")
+})
