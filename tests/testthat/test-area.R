a46ddcf9b96af77ee88d155322f0c4a9741cc2ca
Yue2901 <80172395+Yue2901@users.noreply.github.com>
2021-11-28 18:52:00 +0100

    Update package tests
    Corrected the tests for the package

--- tests/testthat/test-area.R
+++ tests/testthat/test-area.R
@@ -1,6 +1,6 @@
 # Test whether the plot (i.e. output) is visible
 test_that("pkghw4g6 works", {
-  area <- pkghw4v2::estimate_area()
+  area <- pkghw4g6::estimate_area()
   plot_shape <- pkghw4g6::plot_area(area)
   expect_visible(plot_shape)
 })
@@ -8,6 +8,11 @@ test_that("pkghw4g6 works", {
 # Test inputs provided
 test_that("input check", {
   expect_error(estimate_area(B = 0, seed = 10))
+})
+
+# Test inputs provided
+test_that("input check", {
+  expect_type(estimate_area(B = 1000, seed = 10), "list")
 })
 
 
